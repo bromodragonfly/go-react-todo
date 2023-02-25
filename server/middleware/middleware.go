101b65bf74eac47fa0f0a2d43ffb9007c0332346
bromodragonfly <87935674+bromodragonfly@users.noreply.github.com>
2023-02-25 21:27:17 +0300

    first commit

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,7 +1,17 @@
 package middleware
 
-import("context" "github.com/joho/godotenv" "github.com/gorilla/mux" "encoding/json" "fmt" "net/http" "log" "go.mongodb.org/mongo-driver/bson" "go.mongodb.org/mongo-driver/bson/primitive" "go.mongodb.org/mongo-driver/mongo" "go.mongodb.org/mongo-driver/mongo/options"
-"os")
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+);
 
 var collection *mongo.Collection
 
@@ -9,13 +19,42 @@ func init() {
 	loadTheEnv()
 	createDBInstance()
 }
+ 
 func loadTheEnv() {
   err :=	godotenv.Load(".env")
-  if err!==nil{
+  if err!=nil{
 	log.Fatal("Env error loading")
   }
 }
-func GetAllTasks(w http.ResponceWritter,r *http.Request) {
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collecntionName := os.Getenv("COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+	
+	err = client.Ping(context.TODO(), nil)
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database(dbName).Collection(collecntionName)
+
+	fmt.Println("Collection instance created")
+	
+}
+
+func GetAllTasks(w http.ResponseWriter,r *http.Request) {
 
 }
 
